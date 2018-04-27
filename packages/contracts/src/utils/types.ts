34b1e82e77d754d590533f502884b50d542b583a
Jacob Evans <jacob@dekz.net>
2018-04-27 16:59:04 +1000

    Update to latest order format

--- packages/contracts/src/utils/types.ts
+++ packages/contracts/src/utils/types.ts
@@ -113,6 +113,7 @@ export enum ContractName {
     TestLibBytes = 'TestLibBytes',
     Authorizable = 'Authorizable',
     Whitelist = 'Whitelist',
+    Forwarder = 'Forwarder',
 }
 
 export enum SignatureType {
